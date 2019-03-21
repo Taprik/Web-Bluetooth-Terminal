23338c2d35778f2c7ae58f156f937c5d4958e98c
Taprik <taprik@taprik.net>
2019-03-21 09:15:10 +0100

    Update BluetoothTerminal.js

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -243,6 +243,7 @@ class BluetoothTerminal {
 
     return navigator.bluetooth.requestDevice({
       filters: [{services: [this._serviceUuid]}],
+      {acceptAllDevices: true}
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
